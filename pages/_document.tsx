b8c1552d2b2ae7c0f83b11f3333335d5f4061d85
Rosher Mur <rogermur1990@gmail.com>
2025-12-24 11:09:52 -0500

    Feat: Add Google Analytics 4 (G-GF57274CF0)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,18 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bangers&family=Outfit:wght@100..900&family=JetBrains+Mono:wght@100..800&family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+                {/* Google Analytics 4 */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-GF57274CF0"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-GF57274CF0');
+                        `,
+                    }}
+                />
             </Head>
             <body className="bg-paper text-ink font-body grain-overlay">
                 <Main />
